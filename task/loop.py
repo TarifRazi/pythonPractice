d40454b86059df94f263adfc2718b802e7c97b4d
TarifRazi <tarifuddinrajee@gmail.com>
2026-01-05 10:05:28 +0600

    loop compleated

--- task/loop.py
+++ task/loop.py
@@ -3,6 +3,16 @@ for i in range(0,10):
 
 print("-----")
 
-for e in range(0,21):
+for e in range(0,20):
     if e % 2 == 0:
         print(e)
+
+print("-----")
+
+num = int(input("Enter a number: "))
+for i in range(1,11):
+    print(num, "x", i, "=", num * i)
+print("-----")
+
+for r in range(5,0,-1):
+    print(r)
